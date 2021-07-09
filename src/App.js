7fe6bfaddc449b1e96ca17667f7593cb4109fac1
juhoon-lee <wngns8334@gmail.com>
2021-07-09 21:49:05 +0900

    Router before css

--- src/App.js
+++ src/App.js
@@ -1,58 +1,18 @@
 import React from 'react';
-import axios from 'axios';
-import Movie from './Movie';
-import "./App.css";
-// 함수형 컴포넌트
-// function App() {
-//   return(
-//     <h1>Movie App</h1>
-//   );
-// }
+import {HashRouter, Route} from "react-router-dom";
+import Home from "./routes/Home";
+import About from "./routes/About";
+import Navigation from "./components/Navigation";
 
-// 클래스형 컴포넌트
-class App extends React.Component {
-  state= {
-    isLoading : true,
-    movies : [], // state에 데이터를 미리 선언하지 않아도 괜찮지만 체계적인 코드 정리를 위해선 미리 계획하는게 좋다.
-  };
 
-  getMovies = async () => {
-    const {
-      data: {
-        data: {movies}
-      }
-    } = await axios.get("https://yts-proxy.nomadcoders1.now.sh/list_movies.json?sort_by=rating");
-    this.setState({movies : movies, isLoading: false}); // {movies:movies}를 {movies}라고 es6에서 쓰는게 더 좋다.
-  };
-
-  componentDidMount(){
-    this.getMovies();
-  }
-
-  render(){ // render 함수가 JSX를 리턴
-    const {isLoading, movies} = this.state;
-    return <section className="container">
-      {isLoading
-          ? <div className="loader">
-              <span className="loader_text">Loading...</span>
-          </div>
-            :
-            <div className="movies">
-            {movies.map(movie => {
-              return <Movie
-                  key={movie.id}
-                  id={movie.id}
-                  year={movie.year}
-                  title={movie.title}
-                  summary={movie.summary} 
-                  poster={movie.medium_cover_image}
-                  genres={movie.genres}
-                  />
-                })}
-            </div>
-    }</section>;
-  }
+function App() {
+  return (
+    <HashRouter>
+      <Navigation />
+      <Route path="/" exact={true} component={Home}/>
+      <Route path="/about" component={About}/>
+    </HashRouter>
+  );
 }
 
-export default App;
-// https://yts-proxy.nomadcoders1.now.sh/list_movies.json
+export default App
---
+++ src/routes/Home.js
@@ -0,0 +1,59 @@
+import React from 'react';
+import axios from 'axios';
+import Movie from '../components/Movie';
+import "./Home.css";
+// 함수형 컴포넌트
+// function App() {
+//   return(
+//     <h1>Movie App</h1>
+//   );
+// }
+
+// 클래스형 컴포넌트
+class Home extends React.Component {
+  state= {
+    isLoading : true,
+    movies : [], // state에 데이터를 미리 선언하지 않아도 괜찮지만 체계적인 코드 정리를 위해선 미리 계획하는게 좋다.
+  };
+
+  getMovies = async () => {
+    const {
+      data: {
+        data: {movies}
+      }
+    } = await axios.get("https://yts-proxy.nomadcoders1.now.sh/list_movies.json?sort_by=rating");
+    this.setState({movies : movies, isLoading: false}); // {movies:movies}를 {movies}라고 es6에서 쓰는게 더 좋다.
+  };
+
+  componentDidMount(){
+    this.getMovies();
+  }
+
+  render(){ // render 함수가 JSX를 리턴
+    const {isLoading, movies} = this.state;
+    return <section className="container">
+      {isLoading
+          ? <div className="loader">
+              <span className="loader_text">Loading...</span>
+          </div>
+            :
+            <div className="movies">
+            {movies.map(movie => {
+              return <Movie
+                  key={movie.id}
+                  id={movie.id}
+                  year={movie.year}
+                  title={movie.title}
+                  summary={movie.summary} 
+                  poster={movie.medium_cover_image}
+                  genres={movie.genres}
+                  />
+                })}
+            </div>
+    }</section>;
+  }
+}
+
+export default Home
+;
+// https://yts-proxy.nomadcoders1.now.sh/list_movies.json
